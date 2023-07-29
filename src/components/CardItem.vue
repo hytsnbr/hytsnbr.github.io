09f5e461f88fdc5377fff4c8d68329db04c3c4c3
hytsnbr10519 <haya524mwam@gmail.com>
2023-07-29 09:57:48 +0900

    chore: ハイレゾ対応可否を表示するようにした

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -10,7 +10,7 @@ defineProps<Props>();
 
 <template>
   <div class="card card-side h-64 w-[488px] bg-base-100 px-5 shadow-xl">
-    <div class="card-body  px-0 py-3" :class="cdInfo.limited ? '' : 'justify-end'">
+    <div class="card-body px-0 py-3" :class="cdInfo.limited ? '' : 'justify-end'">
       <h2 class="card-title h-1/3 grow justify-center text-base">{{ cdInfo.title }}</h2>
       <div class="mt-5">
         <p>{{ cdInfo.artist }}</p>
@@ -18,12 +18,14 @@ defineProps<Props>();
         <p>{{ cdInfo.recordNumbers.join(", ") }}</p>
       </div>
       <div class="mt-5 flex grow justify-center gap-6">
-        <div v-if="cdInfo.downloadSiteList.length > 0" class="dropdown-hover dropdown">
+        <div v-if="cdInfo.downloadSiteList.length > 0" class="dropdown dropdown-hover">
           <label tabindex="0" class="download-site btn w-52">ダウンロードサイト</label>
           <ul class="menu dropdown-content rounded-box z-[1] w-auto bg-base-100 p-2 shadow">
             <template v-for="storeSite in cdInfo.downloadSiteList" :key="storeSite">
               <li>
-                <a :href="storeSite.url" target="_blank">{{ storeSite.name }} </a>
+                <a :href="storeSite.url" target="_blank"
+                  >{{ storeSite.name }}{{ storeSite.isHiRes ? "（ハイレゾ）" : "" }}</a
+                >
               </li>
             </template>
           </ul>
@@ -33,7 +35,9 @@ defineProps<Props>();
           <ul class="menu dropdown-content rounded-box z-[1] w-auto bg-base-100 p-2 shadow">
             <template v-for="storeSite in cdInfo.purchaseSiteList" :key="storeSite">
               <li>
-                <a :href="storeSite.url" target="_blank">{{ storeSite.name }} </a>
+                <a :href="storeSite.url" target="_blank"
+                  >{{ storeSite.name }}{{ storeSite.isHiRes ? "（ハイレゾ）" : "" }}
+                </a>
               </li>
             </template>
           </ul>
